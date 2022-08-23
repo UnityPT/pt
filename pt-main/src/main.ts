364cf04864d88a39df84ad92aa42d17d18fd4065
frogeater <shiwashou59@gmail.com>
2022-08-23 21:07:38 +0800

    显示控制台

--- pt-main/src/main.ts
+++ pt-main/src/main.ts
@@ -59,7 +59,7 @@ function createWindow() {
     mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(path.join(__dirname, '..', 'pt-main-web/index.html'));
-    // mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
   }
 }
 
