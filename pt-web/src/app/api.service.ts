31a448e07e6c94ac061c37443f3b041a63912dce
frogeater <shiwashou59@gmail.com>
2022-10-18 22:26:48 +0800

    1.0.5.4

--- pt-web/src/app/api.service.ts
+++ pt-web/src/app/api.service.ts
@@ -62,6 +62,7 @@ export class ApiService {
       url.searchParams.append('t', Date.now().toString());
     }
 
+
     let body = [] as Resource[];
     try {
        const ret = await lastValueFrom(this.http.get<Resource[]>(url.href, {
