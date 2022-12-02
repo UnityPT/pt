f1b51e83927d1567136e900c6fc68341665dabec
frogeater <shiwashou59@gmail.com>
2022-12-02 23:29:23 +0800

    Revert "打包test"
    This reverts commit 7e1e54681aae1b61261f14b17e0a11b566dbfbe0.

--- pt-main/src/ssh.ts
+++ pt-main/src/ssh.ts
@@ -1,45 +1,45 @@
-// import { Client, ConnectConfig } from 'ssh2';
+import { Client, ConnectConfig } from 'ssh2';
 import { readFileSync } from 'fs';
 import { webContents } from 'electron';
 import { electronAPI } from './electronAPI';
 import path from 'path';
 
-// export class SSH {
-//   conn = new Client();
-//
-//   async createConnect() {
-//     const cfg = await electronAPI.store.get('sshConfig');
-//     const portExists = cfg.remotePath.split(':').length > 1;
-//     const connectCfg: ConnectConfig = {
-//       privateKey: readFileSync(cfg.privateKeyPath),
-//       username: cfg.username,
-//       host: cfg.remotePath.split(':')[0],
-//       port: portExists ? parseInt(cfg.remotePath.split(':')[1]) : 22,
-//     };
-//     return new Promise((resolve, reject) => {
-//       this.conn.on('ready', () => {
-//         resolve(true);
-//       }).on('error', (err) => {
-//         reject(err);
-//       }).connect(connectCfg);
-//     });
-//   }
-//
-//   async getFile(remotePath: string, localPath: string, infoHash: string) {
-//     this.conn.sftp((err, sftp) => {
-//       if (err) throw err;
-//       sftp.fastGet(remotePath, localPath, {
-//         step: (total_transferred, chunk, total) => {
-//           console.log(total_transferred, chunk, total);
-//           webContents.getFocusedWebContents().send('get_file_progress', {
-//             infoHash,
-//             progress: total_transferred / total,
-//           });
-//         },
-//       }, (err) => {
-//         if (err) throw err;
-//         this.conn.end();
-//       });
-//     });
-//   }
-// }
+export class SSH {
+  conn = new Client();
+
+  async createConnect() {
+    const cfg = await electronAPI.store.get('sshConfig');
+    const portExists = cfg.remotePath.split(':').length > 1;
+    const connectCfg: ConnectConfig = {
+      privateKey: readFileSync(cfg.privateKeyPath),
+      username: cfg.username,
+      host: cfg.remotePath.split(':')[0],
+      port: portExists ? parseInt(cfg.remotePath.split(':')[1]) : 22,
+    };
+    return new Promise((resolve, reject) => {
+      this.conn.on('ready', () => {
+        resolve(true);
+      }).on('error', (err) => {
+        reject(err);
+      }).connect(connectCfg);
+    });
+  }
+
+  async getFile(remotePath: string, localPath: string, infoHash: string) {
+    this.conn.sftp((err, sftp) => {
+      if (err) throw err;
+      sftp.fastGet(remotePath, localPath, {
+        step: (total_transferred, chunk, total) => {
+          console.log(total_transferred, chunk, total);
+          webContents.getFocusedWebContents().send('get_file_progress', {
+            infoHash,
+            progress: total_transferred / total,
+          });
+        },
+      }, (err) => {
+        if (err) throw err;
+        this.conn.end();
+      });
+    });
+  }
+}
